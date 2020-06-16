86b59df71f0a15f185238b1ceb6e5fad3401c60b
Megan Kim <33720329+kimjungsun@users.noreply.github.com>
2020-06-16 14:27:04 +0900

    dual _client

--- client.c
+++ client.c
@@ -4,76 +4,74 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include<pthread.h>
+#include<string.h>
 void error_handling(char *message);
-void *thr_read(void *arg);
-void *thr_write(void *arg);
 
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-        int sock;
-        struct sockaddr_in serv_addr;
-
-        int status ;
-        pthread_t tid[5];
-        int state ; // pthread_join parameter
-
-        sock = socket(PF_INET, SOCK_STREAM, 0);
-        if (sock == -1)
-               error_handling("socket() error");
-
-        memset(&serv_addr, 0, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-        serv_addr.sin_port = htons(atoi(argv[2]));
-
-        if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-               error_handling("connect() error");
-
-        if((status = pthread_create(&tid[0],NULL,&thr_read,(void *)&sock))!=0){
-			printf("READ THREAD CREATE FAIL !\n");
-			exit(0);
-			}
-        if((status = pthread_create(&tid[1],NULL,&thr_write,(void *)&sock))!=0){
-       			printf("READ THREAD CREATE FAIL !\n");
-       			exit(0);
-       			}
-
-        pthread_join(tid[0],(void **)&state);
-
-        pthread_join(tid[1],(void **)&state);
-
-        close(sock);
-                return 0;
-        }
-
-void *thr_write(void *arg){
-	int sock = (int)*((int *)arg);
-        char buffer;	
-	while(1){
-	     scanf("%c",&buffer);
-		if(buffer == '0')
-		{
-	       printf(" sending '0'.... now! \n");
-              write(sock,&buffer,sizeof(buffer));
-	      fflush(stdin);    //혹은 scanf(" %c", &buffer); 로 위의 코드 수정해도 됨.
-		}
+	int sock,sock2;
+	struct sockaddr_in serv_addr,serv_addr2;
+	char c_message[10], c_message2[10];
+	char message[20] = " Well transported!";
+	int str_len, str_len2;
+	if (argc != 3)
+	{
+		printf("7.16 / local 각 port 2개 입력해주세용 \n");
+		exit(1);
 	}
+
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = inet_addr("192.168.7.16");
+	serv_addr.sin_port = htons(atoi(argv[1]));
+
+	memset(&serv_addr2, 0, sizeof(serv_addr2));
+	serv_addr2.sin_family = AF_INET;
+	serv_addr2.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serv_addr2.sin_port = htons(atoi(argv[2]));
+
+
+      for(int i = 0 ; i<10; i++){
+
+	sock = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
+	sock2 = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
+	
+	if (sock == -1 || sock2 == -1)
+		error_handling("socket() error");
+
+	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+		error_handling("connect() error");
+	if (connect(sock2, (struct sockaddr*)&serv_addr2, sizeof(serv_addr2)) == -1)
+		error_handling("connect() error");
+
+	str_len = read(sock,c_message, sizeof(c_message) - 1);
+	str_len2 = read(sock2,c_message2, sizeof(c_message2) - 1);
+	if (str_len == -1)
+		error_handling("read()--1 error");
+	if (str_len2 == -1)
+		error_handling("read()--2 error");
+        write(sock,message,sizeof(message));
+	strcat(c_message, c_message2);
+	printf("Message from both server:%s\n", c_message);
+	printf("Message from local server:%s\n", c_message2);
+close(sock);
+close(sock2);
+}	
+
+	return 0;
+
 }
-void *thr_read(void *arg){
-    char recv ;
-	int sock = (int)*((int *)arg);
-	while(1){
-	if(read(sock,&recv, sizeof(recv))!=0)
-            printf("Recieve from server:%c\n" ,recv);
-	}
-}
+
+
 
 void error_handling(char *message)
+
 {
-        fputs(message, stderr);
-        fputc('\n', stderr);
-        exit(1);
+
+	fputs(message, stderr);
+
+	fputc('\n', stderr);
+
+	exit(1);
 
 }
